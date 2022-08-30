d165722f306664c4003277d9ea240849748e8153
Vismay <vismayp250703@gmail.com>
2022-08-30 15:07:19 +0530

    first commit

---
+++ app/src/main/java/com/example/testapp/MessageModel.java
@@ -0,0 +1,34 @@
+package com.example.testapp;
+
+public class MessageModel {
+    private String message;
+    private String from;
+    private String keyUnique;
+
+    public MessageModel() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getKeyUnique() {
+        return keyUnique;
+    }
+
+    public void setKeyUnique(String keyUnique) {
+        this.keyUnique = keyUnique;
+    }
+}
